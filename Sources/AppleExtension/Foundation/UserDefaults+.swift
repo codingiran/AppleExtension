3146dfdaf466e636f2b22bb68b87fd70e94c6c42
CodingIran <CodingIran@gmail.com>
2023-09-08 09:58:26 +0800

    Release 2.0.6

---
+++ Sources/AppleExtension/Foundation/UserDefaults+.swift
@@ -0,0 +1,25 @@
+//
+//  UserDefaults+.swift
+//  AppleExtension
+//
+//  Created by iran.qiu on 2023/9/8.
+//
+
+import Foundation
+
+public extension UserDefaults {
+    func setCodableValue<T>(_ value: T, for key: String) where T: Codable {
+        guard let data = try? JSONEncoder().encode(value),
+              let json = try? JSONSerialization.jsonObject(with: data)
+        else { return }
+        set(json, forKey: key)
+    }
+
+    func getCodableValue<T>(of type: T.Type, for key: String) -> T? where T: Codable {
+        guard let json = object(forKey: key),
+              let data = try? JSONSerialization.data(withJSONObject: json),
+              let value = try? JSONDecoder().decode(T.self, from: data)
+        else { return nil }
+        return value
+    }
+}
