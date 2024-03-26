8bb01890e6d27312328c3dde211b18c7ce9badee
CodingIran <CodingIran@gmail.com>
2024-03-26 14:55:24 +0800

    Bump version 2.1.6

--- Sources/AppleExtension/AppleExtension.swift
+++ Sources/AppleExtension/AppleExtension.swift
@@ -12,5 +12,5 @@ import Foundation
 #error("AppleExtension doesn't support Swift versions below 5.5.")
 #endif
 
-/// Current AppleExtension version 2.1.5. Necessary since SPM doesn't use dynamic libraries. Plus this will be more accurate.
-let version = "2.1.5"
+/// Current AppleExtension version 2.1.6. Necessary since SPM doesn't use dynamic libraries. Plus this will be more accurate.
+let version = "2.1.6"
